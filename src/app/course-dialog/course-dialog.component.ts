d1df7088298d04dc69bbf642719af9e9d4597c99
joaquinmal <48302550+joaquinmal@users.noreply.github.com>
2021-08-18 12:30:51 -0500

    Operators finished

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -3,17 +3,16 @@ import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import * as moment from 'moment';
-import {fromEvent, noop} from 'rxjs';
-import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap, tap} from 'rxjs/operators';
+import {fromEvent} from 'rxjs';
+import {concatMap, distinctUntilChanged, exhaustMap, filter, mergeMap} from 'rxjs/operators';
 import {fromPromise} from 'rxjs/internal-compatibility';
-import {Store} from '../common/store.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css']
 })
-export class CourseDialogComponent implements AfterViewInit {
+export class CourseDialogComponent implements OnInit, AfterViewInit {
 
     form: FormGroup;
 
@@ -26,8 +25,7 @@ export class CourseDialogComponent implements AfterViewInit {
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
-        @Inject(MAT_DIALOG_DATA) course:Course,
-        private store:Store) {
+        @Inject(MAT_DIALOG_DATA) course:Course ) {
 
         this.course = course;
 
@@ -40,18 +38,39 @@ export class CourseDialogComponent implements AfterViewInit {
 
     }
 
+    ngOnInit() {
+
+        this.form.valueChanges
+            .pipe(
+                filter(() => this.form.valid),
+                concatMap(changes => this.saveCourse(changes))
+            )
+            .subscribe();
+
+
+    }
+
+
+    saveCourse(changes) {
+        return fromPromise(fetch(`/api/courses/${this.course.id}`,{
+            method: 'PUT',
+            body: JSON.stringify(changes),
+            headers: {
+                'content-type': 'application/json'
+            }
+        }));
+    }
+
+
     ngAfterViewInit() {
 
-    }
+        fromEvent(this.saveButton.nativeElement, 'click')
+            .pipe(
+                exhaustMap(() => this.saveCourse(this.form.value))
+            )
+            .subscribe();
 
-    save() {
-        this.store.saveCourse(this.course.id, this.form.value)
-            .subscribe(
-                () => this.close(),
-                err => console.log("Error saving course", err)
-            );
     }
-
 
 
 
